f8d9671acc929095ff3f8fe52f0de3c720e0c392
Ashraful Islam <rx.ju.edu@gmail.com>
2021-08-13 00:13:24 +0700

    Ducks pattern

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import { bugAdded, bugResolved, bugRemoved } from "./actions";
-import store from "./store";
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
+
+const store = configureStore();
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(bugAdded("A new bug1"));
-store.dispatch(bugAdded("A new bug2"));
-store.dispatch(bugAdded("A new bug3"));
-store.dispatch(bugResolved(1));
-store.dispatch(bugRemoved(1));
+store.dispatch(actions.bugAdded("A new bug1"));
+store.dispatch(actions.bugAdded("A new bug2"));
+store.dispatch(actions.bugAdded("A new bug3"));
+store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugRemoved(1));
