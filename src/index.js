ab0dcb53b3915cf3dd25b42417a6bb97052c04cd
Ashraful Islam <rx.ju.edu@gmail.com>
2021-08-13 02:49:40 +0700

    :tada: API middleware

--- src/index.js
+++ src/index.js
@@ -11,25 +11,13 @@ import { userAdded } from "./store/users";
 
 const store = configureStore();
 
-// const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-// store.dispatch(userAdded({ name: "User1" }));
-
-// store.dispatch((dispatch, getState) => {
-//   dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
-//   console.log(getState());
-// });
-
-store.dispatch({ type: "error", payload: { message: "123" } });
-
-// store.dispatch(userAdded({ name: "User2" }));
-// store.dispatch(projectAdded({ name: "project" }));
-// store.dispatch(bugAdded({ description: "A new bug1" }));
-// store.dispatch(bugAdded({ description: "A new bug2" }));
-// store.dispatch(bugAdded({ description: "A new bug3" }));
-// store.dispatch(bugResolved({ id: 1 }));
-// store.dispatch(bugRemoved({ id: 3 }));
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-
-// const bugsByUser1 = getBugsByUser(1)(store.getState());
-// console.log(bugsByUser1);
+store.dispatch({
+  type: "apiCallBegan",
+  payload: {
+    url: "/bugs",
+    // method: "get",
+    // data: {},
+    onSuccess: "bugsReceived",
+    onError: "apiRequestFailed",
+  },
+});
