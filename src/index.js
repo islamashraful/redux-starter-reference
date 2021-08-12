0fc33d735bc6bdf17272c7c486ca872c2192b8ec
Ashraful Islam <rx.ju.edu@gmail.com>
2021-08-13 01:48:42 +0700

    A simple logger middleware

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ const store = configureStore();
 // const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(userAdded({ name: "User1" }));
-store.dispatch(userAdded({ name: "User2" }));
-store.dispatch(projectAdded({ name: "project" }));
-store.dispatch(bugAdded({ description: "A new bug1" }));
-store.dispatch(bugAdded({ description: "A new bug2" }));
-store.dispatch(bugAdded({ description: "A new bug3" }));
-store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(bugRemoved({ id: 3 }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(userAdded({ name: "User2" }));
+// store.dispatch(projectAdded({ name: "project" }));
+// store.dispatch(bugAdded({ description: "A new bug1" }));
+// store.dispatch(bugAdded({ description: "A new bug2" }));
+// store.dispatch(bugAdded({ description: "A new bug3" }));
+// store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugRemoved({ id: 3 }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 
-const bugsByUser1 = getBugsByUser(1)(store.getState());
-console.log(bugsByUser1);
+// const bugsByUser1 = getBugsByUser(1)(store.getState());
+// console.log(bugsByUser1);
