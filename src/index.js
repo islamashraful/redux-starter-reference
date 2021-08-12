8af188aa3b0a59b3ab9273d99d80248c198704c6
Ashraful Islam <rx.ju.edu@gmail.com>
2021-08-13 00:28:18 +0700

    Use action creators from redux-toolkit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const store = configureStore();
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(actions.bugAdded("A new bug1"));
-store.dispatch(actions.bugAdded("A new bug2"));
-store.dispatch(actions.bugAdded("A new bug3"));
-store.dispatch(actions.bugResolved(1));
-store.dispatch(actions.bugRemoved(1));
+store.dispatch(actions.bugAdded({ description: "A new bug1" }));
+store.dispatch(actions.bugAdded({ description: "A new bug2" }));
+store.dispatch(actions.bugAdded({ description: "A new bug3" }));
+store.dispatch(actions.bugResolved({ id: 1 }));
+store.dispatch(actions.bugRemoved({ id: 3 }));
