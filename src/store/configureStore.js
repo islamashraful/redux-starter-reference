edc86de3490de41f73c9b12a4919f513465872f5
Ashraful Islam <rx.ju.edu@gmail.com>
2021-08-13 00:27:40 +0700

    Configure store using redux-toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,6 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
-export default function configureStore() {
-  const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
