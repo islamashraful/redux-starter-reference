fba5498e2e82e98660a9feabe5a57b22ca1d7b44
Ashraful Islam <rx.ju.edu@gmail.com>
2021-08-13 01:08:11 +0700

    Combine multiple reducer and restructure store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
+store.dispatch(projectAdded({ name: "project" }));
 store.dispatch(actions.bugAdded({ description: "A new bug1" }));
 store.dispatch(actions.bugAdded({ description: "A new bug2" }));
 store.dispatch(actions.bugAdded({ description: "A new bug3" }));
