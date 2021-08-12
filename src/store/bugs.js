4938a5ddd6a229c5a1f42660ca9ca4b3adf692a2
Ashraful Islam <rx.ju.edu@gmail.com>
2021-08-13 00:37:44 +0700

    Refactor reducer using redux-toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // Action creators
 export const bugAdded = createAction("bugAdded");
@@ -8,24 +8,20 @@ export const bugRemoved = createAction("bugRemoved");
 // Reducer
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-    case bugRemoved.type:
-      return state.filter((bug) => bug.id !== action.payload.id);
-    case bugResolved.type:
-      return state.map((bug) =>
-        bug.id === action.payload.id ? { ...bug, resolved: true } : bug
-      );
-    default:
-      return state;
-  }
-}
+export default createReducer([], {
+  [bugAdded.type]: (state, action) => {
+    state.push({
+      id: ++lastId,
+      description: action.payload.description,
+      resolved: false,
+    });
+  },
+  [bugResolved.type]: (state, action) => {
+    const index = state.findIndex((item) => item.id === action.payload.id);
+    state[index].resolved = true;
+  },
+  [bugRemoved.type]: (state, action) => {
+    const index = state.findIndex((item) => item.id === action.payload.id);
+    state.splice(index, 1);
+  },
+});
